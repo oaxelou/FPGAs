3002a2a72061158ad2104aec76bca201b23953f1
oaxelou <oaxelou@inf.uth.gr>
2018-10-07 18:42:36 +0300

    clk->20ns & reset

--- partB/tb.v
+++ partB/tb.v
@@ -1,5 +1,4 @@
-`timescale 1ns/1ns;
-
+`timescale 1ns/1ns
 module tb;
 
 reg clk;
@@ -10,12 +9,12 @@ wire a,b,c,d,e,f,g,dp;
 
 initial begin
 	clk = 1'b1;
-	#400 reset = 1'b1;
+	#100 reset = 1'b1;
 	#100 reset = ~reset;
 end
 
 always begin
-	#160 clk = ~clk;
+	#10 clk = ~clk;
 end
 
 FourDigitLEDdriver topLevelinstance(reset, clk, an3, an2, an1, an0, a, b, c, d, e, f, g, dp);
