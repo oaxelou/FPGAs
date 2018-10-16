99225e939437542b8b0e91d710ac92bfb7b29b3d
oaxelou <oaxelou@uth.gr>
2018-10-16 11:19:01 +0300

    rst synchr: added comments

--- partB/reset_synchronizer.v
+++ partB/reset_synchronizer.v
@@ -1,16 +1,28 @@
+/* Axelou Olympia
+ * oaxelou@uth.gr 
+ * 2161
+ * 
+ * ce430
+ * Project1: 7-Segment display
+ *
+ * Part B: 
+ *
+ * reset_synchronizer: Synchronizes raw reset signal
+ * 
+ * input : clk, reset
+ * output: syncronized reset
+ */
+
 module reset_synchronizer(clk, reset, new_reset);
 
-input clk, reset;
-output new_reset;
-reg new_reset;
-reg f1_output;
+	input clk, reset;
+	output new_reset;
+	reg new_reset;
+	reg f1_output;
 
-always @(posedge clk) begin
-	f1_output = reset;
-end
-
-always @(posedge clk) begin
-	new_reset = f1_output;
-end
+	always @(posedge clk) begin
+		new_reset = f1_output;
+		f1_output = reset;
+	end
 
 endmodule
