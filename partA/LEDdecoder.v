6a186d1d66c14def784dc44b0302eae1c9a065ae
oaxelou <oaxelou@uth.gr>
2018-10-22 20:13:34 +0300

    Improved comments. Ready

--- partA/LEDdecoder.v
+++ partA/LEDdecoder.v
@@ -11,11 +11,27 @@
  *               (active high i.e. char 'a' : 4'b1010)
  *	      output: the LEDs of the 7-Segment display
  *               (active low)
+ *
+ *
+ * This circuit maps the binary code of a char 
+ * to the segments that must be opened to display 
+ * the char on the seven segment indicator.
+ * (given that a segment is 0 when opened)
+ *
+ * E.g., char 'a' : 
+ *                  binary code: 1010
+ *                  7-S-D  code: 0001000
+ *                                _  
+ *                visual result: | |
+ *                                -
+ *                               | |
  */
 
 module LEDdecoder(in, LED);
 input [3:0] in;
 output [6:0] LED;
+
+reg [6:0] LED;
 
 parameter char0 = 7'b0000001,
           char1 = 7'b1001111,
@@ -33,7 +49,7 @@ parameter char0 = 7'b0000001,
           charD = 7'b1000010,
           charE = 7'b0110000,
           charF = 7'b0111000;
-
+/*
 assign LED = in[3] ? (in[2] ? (in[1] ? (in[0]? charF : charE)  : 
                                        (in[0]? charD : charC)) : 
                               (in[1] ? (in[0]? charB : charA)  : 
@@ -42,5 +58,28 @@ assign LED = in[3] ? (in[2] ? (in[1] ? (in[0]? charF : charE)  :
                                        (in[0]? char5 : char4)) : 
                               (in[1] ? (in[0]? char3 : char2)  : 
                                        (in[0]? char1 : char0)));
+*/
+
+always @(in)
+begin
+	case(in)
+		4'b1111: LED = charF;
+		4'b1110: LED = charE;
+		4'b1101: LED = charD;
+		4'b1100: LED = charC;
+		4'b1011: LED = charB;
+		4'b1010: LED = charA;
+		4'b1001: LED = char9;
+		4'b1000: LED = char8;
+		4'b0111: LED = char7;
+		4'b0110: LED = char6;
+		4'b0101: LED = char5;
+		4'b0100: LED = char4;
+		4'b0011: LED = char3;
+		4'b0010: LED = char2;
+		4'b0001: LED = char1;
+		4'b0000: LED = char0;
+	endcase
+end
 
 endmodule
---
+++ partA/LEDdecoder_tb.v
@@ -0,0 +1,191 @@
+`timescale 1ns / 10ps
+
+/* Axelou Olympia
+ * oaxelou@uth.gr 
+ * 2161
+ * 
+ * ce430
+ * Project1: 7-Segment display
+ *
+ * Part A: 7-Segment decoder implementation
+ * 
+ * LEDdecoder_testbench: tests LEDdecoder with the 16 possible inputs
+ *                       and uses a counter for the correct outputs
+ * For the 16 cases:
+ * -> Waits 1ns
+ * -> Sets stdout and input
+ * -> Waits 1ns
+ * -> displays time,in,out,stdout and checks output
+ */
+
+module LEDdecoder_testbench;
+
+reg [3:0] in;
+wire [6:0] out;
+reg [6:0] stdout;
+
+integer correct_cases;
+
+parameter char0 = 7'b0000001,
+          char1 = 7'b1001111,
+          char2 = 7'b0010010,
+          char3 = 7'b0000110,
+          char4 = 7'b1001100,
+          char5 = 7'b0100100,
+          char6 = 7'b0100000,
+          char7 = 7'b0001111,
+          char8 = 7'b0000000,
+          char9 = 7'b0000100,
+          charA = 7'b0001000,
+          charB = 7'b1100000,
+          charC = 7'b1110010,
+          charD = 7'b1000010,
+          charE = 7'b0110000,
+          charF = 7'b0111000;
+
+LEDdecoder decoder_inst(.in(in), .LED(out));
+
+initial 
+begin
+	correct_cases = 0;
+
+	//1st
+	stdout = char0;
+	in = 4'b0000;	
+
+	#1 $display("time = %t, in: %b, out: %b, stdout: %b, stdout and myout are the same: %b\n", $time, in, out, stdout, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+	// 2nd
+	#1 stdout = char1;
+	in = in + 1;
+
+	#1 $display("time = %t, in: %b, out: %b, stdout and myout are the same: %b\n", $time, in, out, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+	// 3rd
+	#1 stdout = char2;
+	in = in + 1;
+	
+	#1 $display("time = %t, in: %b, out: %b, stdout and myout are the same: %b\n", $time, in, out, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+	// 4th
+	#1 stdout = char3;
+	in = in + 1;
+
+	#1 $display("time = %t, in: %b, out: %b, stdout and myout are the same: %b\n", $time, in, out, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+	// 5th
+	#1 stdout = char4;
+	in = in + 1;
+
+	#1 $display("time = %t, in: %b, out: %b, stdout and myout are the same: %b\n", $time, in, out, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+	// 6th
+	#1 stdout = char5;
+	in = in + 1;
+
+	#1 $display("time = %t, in: %b, out: %b, stdout and myout are the same: %b\n", $time, in, out, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+	// 7th
+	#1 stdout = char6;
+	in = in + 1;
+
+	#1 $display("time = %t, in: %b, out: %b, stdout and myout are the same: %b\n", $time, in, out, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+	// 8th
+	#1 stdout = char7;
+	in = in + 1;
+
+	#1 $display("time = %t, in: %b, out: %b, stdout and myout are the same: %b\n", $time, in, out, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+	// 9th
+	#1 stdout = char8;
+	in = in + 1;
+
+	#1 $display("time = %t, in: %b, out: %b, stdout and myout are the same: %b\n", $time, in, out, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+
+	// 10th
+	#1 stdout = char9;
+	in = in + 1;
+
+	#1 $display("time = %t, in: %b, out: %b, stdout and myout are the same: %b\n", $time, in, out, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+
+	// 11th
+	#1 stdout = charA;
+	in = in + 1;
+	
+	#1 $display("time = %t, in: %b, out: %b, stdout and myout are the same: %b\n", $time, in, out, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+
+	// 12th
+	#1 stdout = charB;
+	in = in + 1;
+
+	#1 $display("time = %t, in: %b, out: %b, stdout and myout are the same: %b\n", $time, in, out, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+
+	// 13th
+	#1 stdout = charC;
+	in = in + 1;
+
+	#1 $display("time = %t, in: %b, out: %b, stdout and myout are the same: %b\n", $time, in, out, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+
+	// 14th
+	#1 stdout = charD;
+	in = in + 1;
+
+	#1 $display("time = %t, in: %b, out: %b, stdout and myout are the same: %b\n", $time, in, out, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+
+	// 15th
+	#1 stdout = charE;
+	in = in + 1;
+
+	#1 $display("time = %t, in: %b, out: %b, stdout and myout are the same: %b\n", $time, in, out, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+
+	// 16th
+	#1 stdout = charF;
+	in = in + 1;
+
+	#1 $display("time = %t, in: %b, out: %b, stdout and myout are the same: %b\n", $time, in, out, out == stdout);
+	if(out == stdout)
+		correct_cases = correct_cases + 1;
+
+
+	#1 $display("Correct answers: %d / 16\n", correct_cases);
+end
+
+endmodule
