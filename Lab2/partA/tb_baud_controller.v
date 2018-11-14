429e48ebf356cea3ce5add7d2392638804042579
oaxelou <oaxelou@uth.gr>
2018-11-14 20:43:39 +0200

    partA circuit & tb

--- Lab2/partA/tb_baud_controller.v
+++ Lab2/partA/tb_baud_controller.v
@@ -1,6 +1,12 @@
+/*
+ *
+ * Treksto gia 600ns !
+ */
+
 module tb_baud_controller;
 
 reg clk, reset;
+reg [2:0] reg_baud_select;
 
 wire sample_ENABLE;
 
@@ -13,11 +19,27 @@ begin
 
   #1200 reset = 1'b1;
   #100  reset = 1'b0;
+  
+  reg_baud_select = 3'b111;
+  #5000
+  reg_baud_select = 3'b110;
+  #5000
+  reg_baud_select = 3'b101;
+  #5000
+  reg_baud_select = 3'b100;
+  #10000
+  reg_baud_select = 3'b011;
+  #15000
+  reg_baud_select = 3'b010;
+  #30000
+  reg_baud_select = 3'b001;
+  #60000
+  reg_baud_select = 3'b000;
 end
 
 always #10 clk = ~clk;
 
-baud_controller baud_controllerINSTANCE(.reset(reset), .clk(clk), .baud_select(000),
+baud_controller baud_controllerINSTANCE(.reset(reset), .clk(clk), .baud_select(reg_baud_select),
                                         .sample_ENABLE(sample_ENABLE));
 
 endmodule
